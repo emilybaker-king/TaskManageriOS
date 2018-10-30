be7919118655dbc6b7a9bc7840bb6fb5eaaa0979
Emily Baker-King <ebakerking@gmail.com>
2018-10-30 10:44:34 -0500

    Initial Commit

---
+++ TaskManagerAssessmentApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TaskManagerAssessmentApp
+//
+//  Created by Emily Baker-King on 10/30/18.
+//  Copyright © 2018 Emily Baker-King. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TaskManagerAssessmentApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TaskManagerAssessmentApp
+//
+//  Created by Emily Baker-King on 10/30/18.
+//  Copyright © 2018 Emily Baker-King. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
